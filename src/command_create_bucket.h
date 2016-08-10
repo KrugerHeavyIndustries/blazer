b0cb08083782c40eeb61417590abf0860889b713
Chris Kruger <chris.kruger@mokosocialmedia.com>
2016-08-10 22:04:26 +0800

    forgotten command_create_bucket and command_delete_bucket src

---
+++ src/command_create_bucket.h
@@ -0,0 +1,47 @@
+// vim:set et ts=3 sw=3:
+// __  __ ______ _______ _______ _______ ______ 
+// |  |/  |   __ \   |   |     __|    ___|   __ \
+// |     <|      <   |   |    |  |    ___|      <
+// |__|\__|___|__|_______|_______|_______|___|__|
+//        H E A V Y  I N D U S T R I E S
+//
+// Copyright (C) 2016 Kruger Heavy Industries
+// http://www.krugerheavyindustries.com
+// 
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// 
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+
+#ifndef COMMAND_CREATE_BUCKET_H
+#define COMMAND_CREATE_BUCKET_H
+
+#include "command.h" 
+
+namespace khi { 
+namespace command { 
+
+struct CreateBucket : Base { 
+
+   bool valid(size_t wordc);
+
+   int execute(size_t wordc, CommandLine& cmds, BB& aws);
+
+   void printUsage();
+};
+
+} // namespace command
+} // namespace khi
+
+#endif // COMMAND_CREATE_BUCKET_H
